cd4be50165ac963e077fa23c8cac9d09ed4e96a8
nagu <nagu.uk@gmail.com>
2013-12-01 17:47:29 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbeco::Application.config.secret_token = '36c2a23fd0c6e7c95235993d34b5e9a32af4eeefd8b0b9e60bf61248e8367652d3eb30f4d0e524d2ba045e18aff1ecd6371073c497705906b97dfb219dd0c0b9'
